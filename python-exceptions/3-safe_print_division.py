7954ad744a609b6fd92541cdbd9383172191d568
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-14 11:25:33 +0200

    fix: added "inside" in text output

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,6 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Result: {}".format(result))
     except ZeroDivisionError:
-        print("Error: Cannot divide by Zero!")
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
