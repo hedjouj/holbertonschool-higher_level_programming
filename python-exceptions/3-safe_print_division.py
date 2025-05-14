9453b9bd71d28d94cfa3b65c5242e7153b2cdc5d
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-14 11:23:19 +0200

    fix: used .format

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,6 +2,6 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print(f"Result: {result}")
+        print("Result: {}".format(result))
     except ZeroDivisionError:
         print("Error: Cannot divide by Zero!")
