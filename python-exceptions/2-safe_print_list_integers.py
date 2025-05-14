2826eb24f4080f37d4b6c789cf5ac1f574c13d01
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-14 11:16:34 +0200

    fix: correct spacing in safe_print_list_integers function

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
-                count = count +1
+                count = count + 1
     except (ValueError, TypeError):
         pass
     print()
