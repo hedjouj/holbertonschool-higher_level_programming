9fb10e002f8a01d678d069aa519b54c2f322311a
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-16 15:13:13 +0200

    feat: copy the file 2-matrix tu use it in the test file

---
+++ python-test_driven_development/matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Function that divides all elements of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix by a number
+
+    Args:
+        matrix: list of lists of int/float
+        div: number (int or float)
+
+    Returns:
+        new matrix with elements divided and rounded to 2 decimals
+
+    Raises:
+        TypeError, ZeroDivisionError
+    """
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+                                                for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    row_length = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+        for item in row:
+            if not isinstance(item, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(item / div, 2) for item in row] for row in matrix]
