be40a26e90c99c5e5805e1f83e31f9405b21f08d
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-16 14:02:14 +0200

    fix: correct indentation in add_integer function and update module docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+This module provides a function that adds two integers.
+"""
 def add_integer(a, b=98):
     """Adds 2 integers or floats, returns an integer."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-        return int(a) + int(b)
+    return int(a) + int(b)
