da5881e9d484198f6427741fe51819a17bf881ef
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-19 15:32:39 +0200

    feat: Add main script and square class definition

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Mudole 1-square
+Defines a square class.
+"""
+class Square:
+    """Represents a square.
+    """
+    def __init__(self, size):
+        """Initializes the square with a size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+        
