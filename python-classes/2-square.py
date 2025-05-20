2afe137c5d94e1efc6c93d8370945ceb2ba5cd0f
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-20 09:45:46 +0200

    feat: a class square that defines a square

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+un carré
+"""
+
+
+class Square:
+    """
+    Représente un carré basé sur lexo 1
+    """
+    def __init__(self, size=0):
+         """Initializes the square with a size.
+
+         Args:
+         size (int): la taille du carré ne doit pas être un entier négatif.
+
+         Raises:
+         TypeError: If size is not an integer.
+         ValueError: If size is less than 0.
+         """
+         if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+         if size < 0:
+             raise ValueError("size must be >= 0")
+         self.__size = size
