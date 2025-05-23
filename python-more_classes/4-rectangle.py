d08ec9e47da039966f5c26c2d83ad188028981d8
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-23 14:59:03 +0200

    fix: pycodestyle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a Rectangle class with string and repr representation."""
+"""Thismodule defines a Rectangle class with string and repr representation."""
 
 
 class Rectangle:
