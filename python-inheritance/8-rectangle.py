6981f70b14eba89eeeecdd985c7123d33eed2185
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-27 17:13:33 +0200

    feat: 8. Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""reactangle"""
+
+
+class BaseGeometry:
+    """the parent
+    """
+    def area(self):
+        """
+        Méthode qui lève une exception pour indiquer que
+        la zone n'est pas encore implémentée.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """the kid of the parent
+    """
+    def __init__(self, width, height):
+        """instantiation
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
