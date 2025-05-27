491d4408839ec55799dbc3d160f27265e9dad545
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-27 16:10:13 +0200

    feat: Write a class BaseGeometry (based on 6-base_geometry.py).

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Write an empty class Basegeometry
+"""
+
+
+class BaseGeometry:
+    """classe pour forme géométrique
+    """
+
+    def area(self):
+        """raises an exception
+        """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """validate the value
+        """
+        if type(value) is not int:
+            raise TypeError (f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError (f"{name} must be greater than 0")
