7d97af6c90b2fcac2ba45e524b453df23e017d39
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-27 15:29:12 +0200

    feat: 6. Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write an empty class Basegeometry
+"""
+
+
+class BaseGeometry:
+    """classe pour forme géométrique
+    """
+
+    def area(self):
+        """raises an exception
+        """
+        raise Exception("area() is not implemented")
