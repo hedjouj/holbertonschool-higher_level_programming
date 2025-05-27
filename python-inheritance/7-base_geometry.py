de48f5f4f20a29515581a7d6f31f2ba0e0b55b47
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-27 16:21:17 +0200

    fix: put back the old code

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,6 +6,13 @@
 class BaseGeometry:
     """BaseGeometry class with integer_validator method."""
 
+    def area(self):
+        """
+        Méthode qui lève une exception pour indiquer que
+        la zone n'est pas encore implémentée.
+        """
+        raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Validate that value is a positive integer."""
         if type(value) is not int:
