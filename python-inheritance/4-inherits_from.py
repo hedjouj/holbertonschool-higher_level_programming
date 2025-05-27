70417040365f7080d90e3b663a547b16adb726f3
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-27 10:06:17 +0200

    feat: a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a function that returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class ;
+otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ;
+    otherwise False.
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        if isinstance(obj, a_class):
+            return False
