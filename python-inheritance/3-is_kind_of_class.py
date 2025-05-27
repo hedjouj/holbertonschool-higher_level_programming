8c080a2a1b101eac1ca2ff7ccc6f37ec0d12fb17
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-27 09:46:51 +0200

    feat: a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that returns true if the object is an instance or if the object
+is an instance of a class that inherited from, otherwise false
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """"
+    returns true or false depending on the object
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        if isinstance(obj, a_class):
+            return False
