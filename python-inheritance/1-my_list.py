49bb620c7be47e894874cbb5f7c94ee35ee3748c
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-28 10:22:22 +0200

    fix: added 2 lines to get the right output

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,6 @@ class MyList(list):
     """A subclass of list with a method to print the list sorted."""
 
     def print_sorted(self):
-        """Prints the list in ascending sorted order (does not modify the list)."""
-        print(sorted(self))
+        sorted = self.copy()
+        sorted.sort()
+        print(sorted)
