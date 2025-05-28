00b65d3cf445e3342d7bab3b95fc92805e158e0b
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-28 15:53:23 +0200

    fix: pycodestyle

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,37 +1,48 @@
-from abc import ABC, abstractmethod
+#!/usr/bin/env python3
 import math
+from abc import ABC, abstractmethod
 
+
+# Classe abstraite
 class Shape(ABC):
     @abstractmethod
     def area(self):
-         pass
-      
-    
+        pass
+
     @abstractmethod
     def perimeter(self):
         pass
-    
-class Circle(Shape):
-            def __init__(self, radius):
-                self.radius = radius
-            
-            def area(self):
-                 return math.pi * self.radius **2
-            
-            def perimeter(self):
-                 return 2 * math.pi * self.radius
-            
-class Rectangle(Shape):
-        def __init__(self, width, height):
-              self.width = width
-              self.height = height
 
-        def area(self):
-         return self.width * self.height
-        
-        def perimeter(self):
-             return 2 *(self.width + self.height)
-        
+# Classe Circle qui hérite de Shape
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+# Classe Rectangle qui hérite de Shape
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+# Fonction shape_info qui utilise le duck typing
+
+
 def shape_info(shape):
-         print(f"Area: {shape.area()}")
-         print(f"Perimeter: {shape.perimeter()}")
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
