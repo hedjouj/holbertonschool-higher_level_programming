3ab964375a7ccde6404b62cab417ccb02c4bf2f7
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-28 10:20:52 +0200

    fix: corrected the loops

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -21,6 +21,6 @@ class BaseGeometry:
             ValueError: If value is <= 0.
         """
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
