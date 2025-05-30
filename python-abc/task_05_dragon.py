94ba5bcc6db903e805ca602416fec6f7619830b8
hedjouj <messaoui.elhadj@hotmail.fr>
2025-05-30 15:14:47 +0200

    fix: pycodestyle + tasks

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import math
+from abc import ABC, abstractmethod
+
+class SwimMixin:
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        print("The dragon roars!")
