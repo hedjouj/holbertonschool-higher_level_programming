199fb01cd84cbfa04cb7f0399e4cd0c8e57c7cd8
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-04 13:15:14 +0200

    fix: pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,6 +2,6 @@
 
 
 def write_file(filename="", text=""):
-    """function that writes a string to a text file and returns the numbers of char"""
+    """writes a string to a text file and returns the numbers of char"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
