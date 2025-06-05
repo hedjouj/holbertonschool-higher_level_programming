031d447bdb992b7309c51d922386b843096053e6
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-05 16:27:02 +0200

    feat: 10. Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""""""
+
+
+class Student:
+    """Defines a student with filtering for JSON serialization."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary of attributes; filters if attrs is a list of strings."""
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
