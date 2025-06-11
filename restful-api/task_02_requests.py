b1831b9899b4ce950aeb1dec3e415aada49d4a43
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-11 12:03:20 +0200

    fix: added a else loop

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -10,6 +10,8 @@ def fetch_and_print_posts():
         posts = response.json()
         for post in posts:
             print(post['title'])
+    else:
+        print("Failed to retrieve posts.")
 
 def fetch_and_save_posts():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
