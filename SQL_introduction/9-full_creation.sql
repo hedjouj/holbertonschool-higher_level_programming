81abc069190e5fd06dd9e24269b7a68cdf6acebb
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-23 16:03:05 +0200

    feat: ensure second_table is created only if it does not exist

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,11 @@
 -- create a tabe and add multiples rows
-CREATE TABLE second_table(
+CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
 );
+
+insert into second_table (id, name, score) values (1, 'John', 10);
+insert into second_table (id, name, score) values (2, 'Alex', 3);
+insert into second_table (id, name, score) values (3, 'Bob', 14);
+insert into second_table (id, name, score) values (4, 'George', 8);
