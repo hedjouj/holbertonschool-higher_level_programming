0f788d9cbf6c7f04fa86b22cfbfa5db895bcad82
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-26 10:28:14 +0200

    fix: remove alias from states.name in cities by state query

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all cities in database 
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name 
 FROM cities
 JOIN states ON cities.state_id = states.id
-ORDER BY id ASC;
+ORDER BY cities.id;
