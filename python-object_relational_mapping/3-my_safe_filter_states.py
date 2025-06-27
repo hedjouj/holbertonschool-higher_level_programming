a1f92099330312eee25f7b9d4c0e2cf1654c5a32
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-27 18:09:07 +0200

    feat: implement script to fetch and display states from the database based on user input

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1], passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
+    [print(row) for row in cur.fetchall()]
+    cur.close()
+    conn.close()
