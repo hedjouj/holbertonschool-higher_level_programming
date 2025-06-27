97d00d4c7ab8aaede5da39e302fe543dffe292a2
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-27 14:12:03 +0200

    feat: add script to list all states from the database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Results are sorted by states.id in ascending order.
+"""
+
+
+import MySQLdb
+import sys
+
+
+def list_states(username, password, db_name):
+    """Connects to a MySQL database and retrieves all rows from the 'states' table.
+    The results are sorted in ascending order by the column 'id'."""
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, db=db_name
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    list_states(sys.argv[1], sys.argv[2], sys.argv[3])
