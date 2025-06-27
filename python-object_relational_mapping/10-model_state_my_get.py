4b6a60f7fb552531d763246524fdf2375c8a3a05
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-27 18:09:39 +0200

    feat: add script to fetch and display state ID by name from the database

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    print(state.id if state else "Not found")
+    session.close()
