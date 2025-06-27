08d139c53afa69d2df9bd601b7e4e08ffadbecf4
hedjouj <messaoui.elhadj@hotmail.fr>
2025-06-27 18:34:29 +0200

    refactor: restructure insert_state function for improved readability and parameterization

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
+"""
+This module inserts a new State object (Louisiana) into the database
+and prints the new record's id.
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
 
-if __name__ == "__main__":
+def insert_state(username, password, db_name):
+    """
+    Connects to the database using SQLAlchemy, inserts 'Louisiana'
+    into 'states', commits, and prints the new id.
+    """
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
+        f"mysql+mysqldb://{username}:{password}@localhost/{db_name}",
+        pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name="Louisiana")
-    session.add(new_state)
+    new = State(name="Louisiana")
+    session.add(new)
     session.commit()
-    session.refresh(new_state)  # Ensure new_state.id is populated
-    print(new_state.id)
+    print(new.id)
     session.close()
+
+
+if __name__ == "__main__":
+    insert_state(sys.argv[1], sys.argv[2], sys.argv[3])
