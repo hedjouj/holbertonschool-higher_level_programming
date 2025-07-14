ac3d4a9c4584500cc185e3b38885e287c9c7a01b
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-14 13:52:32 +0200

    feat: 2. Arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that prints a message depending of the number of arguments passed
+
+const argsCount = process.argv.lenght - 2;
+
+if (argsCount === 0) {
+  console.log('No argument');
+} else if (argsCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
