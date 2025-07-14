da9b857bae4968940e06023c079229f1443be57c
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-14 15:01:39 +0200

    fix: fix to make the checker work for 3 argmuments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -6,5 +6,5 @@ if (process.argv.lenght === 2) {
 } else if (process.argv.lenght === 3) {
   console.log('Argument found');
 } else {
-  console.log('Argument found');
+  console.log('Arguments found');
 }
