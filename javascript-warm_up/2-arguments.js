849578cf45f5c3567ae35caf7ed1fc2bf0202d49
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-14 15:01:08 +0200

    fix: fix to make the checker work for 3 argmuments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 // a script that prints a message depending of the number of arguments passed
 
-const argsCount = process.argv.lenght - 2;
-
-if (argsCount === 0) {
+if (process.argv.lenght === 2) {
   console.log('No argument');
-} else if (argsCount === 1) {
+} else if (process.argv.lenght === 3) {
   console.log('Argument found');
 } else {
   console.log('Argument found');
