86da5b3e310c0c30c6e258ec114d50affafc7063
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-15 10:23:31 +0200

    feat: 3. Toggle classes

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,5 @@
+const header = document.getElementById('header');
+const toggleButton = document.getElementById('toggle_header');
+toggleButton.addEventListener('click', function() {
+    header.classList.toggle('hidden');
+});
